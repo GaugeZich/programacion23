5663a43ea5d17e1963f274c3de012e71d0341184
GaugeZich <guilleop.vale@gmail.com>
2023-08-04 11:06:35 -0300

    Creamos una interfaz tipo tarjeta con sus propiedades

--- src/app/modules/inicio/pages/inicio/inicio.component.ts
+++ src/app/modules/inicio/pages/inicio/inicio.component.ts
@@ -7,12 +7,22 @@ import { TarjetasInicio } from 'src/app/models/modelos';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
+  //Propiedad publica (Tipo array)
   public info: TarjetasInicio[];
 
+  //Inicializa la propiedad info
   constructor(){
-    this.info = []
+    this.info = [
+      {
+        titulo:"Tarjeta 1",
+        descripcion:"Jugador lateral o extremo de boca, viene de las inferiores y la esta rompiendo en la primera del futbol argentino",
+        imagen:"https://fotos.perfil.com/2023/05/19/trim/720/410/barco-1571619.jpg",
+        alt:"Valentin \"Colo\" Barco"
+      }
+    ]
   }
 
+  //Evento de construccion / inicializacion
   ngOnInit(): void{
 
   }
